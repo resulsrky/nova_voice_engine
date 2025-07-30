285abaf793fcb11ac0d7cfe81354dcea9668b805
Abdurrahman <abdurrahmankaradag19@gmail.com>
2025-07-30 17:57:52 +0100

    Remove build artifacts and improve noise suppression

--- build/CMakeFiles/voice_engine.dir/src/streaming/slicer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/voice_engine.dir/src/streaming/slicer.cpp.o: \
- /home/raulmoriarty/CLionProjects/voice_engine/src/streaming/slicer.cpp \
- /usr/include/stdc-predef.h
--- include/processing/noise_suppressor.hpp
+++ include/processing/noise_suppressor.hpp
@@ -7,10 +7,12 @@
 namespace processing {
     class NoiseSuppressor {
     public:
-        explicit NoiseSuppressor(int16_t threshold = 500);
-        void process(std::vector<int16_t>& samples) const;
+        explicit NoiseSuppressor(int16_t initial_threshold = 500, float alpha = 0.95f);
+        void process(std::vector<int16_t>& samples);
     private:
-        int16_t threshold_;
+        float threshold_;
+        float noise_level_;
+        float alpha_;
     };
 }
 
